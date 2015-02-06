e67107530882d44d529f4bcf2c30aff5e36d3da2
ShMcK <Shawn.McKay@hotmail.com>
2015-02-06 15:27:33 +0900

    html fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,28 @@ var gulp = require('gulp'),
 	$ = require('gulp-load-plugins')();
 
 var project = {
-	moduleName: 'angular.formly',
-	src: './src/**/*.html',
+	moduleName: 'formly.lumx',
+	src: ['./src/fields/*.html', './src/wrappers/*.html'],
 	dest: './build/templates.js',
 	module: './src/modules/angular-formly-templates-lumx.js',
-	moduleMin: './src/modules/angular-formly-templates-lumx.min.js'
+	minModule: './src/modules/angular-formly-templates-lumx.min.js'
 };
 
 gulp.task('templates', ['clean'], function () {
 	gulp.src(project.src)
-		.pipe($.htmlmin({collapseWhitespace: true}))
+		.pipe($.htmlMinifier({collapseWhitespace: true}))
 		.pipe($.angularTemplatecache())
 		//.pipe($.uglify())
 		.pipe(gulp.dest('./build'));
 });
 
-gulp.task('minTemplates', function () {
+gulp.task('minify', function () {
 	del([
 		project.moduleMin
 	]);
 	gulp.src(project.module)
 		.pipe($.uglify())
-		.pipe(project.moduleMin);
+		.pipe(project.minModule);
 });
 
 gulp.task('clean', function (cb) {
