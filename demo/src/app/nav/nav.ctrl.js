1ab3e2f22cdaefab42521a162ba4f686106fd5e2
ShMcK <Shawn.McKay@hotmail.com>
2015-02-06 11:41:32 +0900

    sidebar directive

--- demo/src/app/nav/nav.ctrl.js
+++ demo/src/app/nav/nav.ctrl.js
@@ -7,7 +7,7 @@
   function NavCtrl(SidebarService) {
     var vm = this;
     vm.sidebar = SidebarService;
-    vm.outLinks = [{
+    vm.navBarLinks = [{
       text: 'Angular-Formly',
       link: 'https://github.com/formly-js/angular-formly'
     }, {
@@ -16,45 +16,6 @@
     }, {
       text: 'GitHub',
       link: 'https://github.com/formly-js/angular-formly-templates-lumx'
-    }];
-
-    vm.navLinks = [{
-      state: 'text',
-      text: 'Text'
-      //icon: 'text-format'
-    }, {
-      state: 'text',
-      text: 'Numbers'
-      //icon: 'plus-one'
-    }, {
-      state: 'option',
-      text: 'Checkboxes'
-      //icon: 'check-box'
-    }, {
-      state: 'option',
-      text: 'Switches'
-      //icon: 'radio-button-off'
-    }, {
-      state: 'option',
-      text: 'Radio buttons'
-      //icon: 'radio-button-on'
-    }, {
-      state: 'select',
-      text: 'Selectors'
-      //icon: 'arrow-drop-down'
-    }, {
-      state: 'select',
-      text: 'Dates'
-      //icon: 'today'
-    }, {
-      state: 'wrapper',
-      text: 'Wrapper'
-    }, {
-      state: 'themes',
-      text: 'Themes'
-    }, {
-      state: 'flex-box',
-      text: 'FlexBox'
     }];
   }
 
---
+++ demo/src/app/nav/sidebar.directive.js
@@ -0,0 +1,62 @@
+(function () {
+  'use strict';
+  angular.module('demo')
+    .directive('shmckSidebar', shmckSidebar);
+
+  function shmckSidebar() {
+
+    var sidebarLinks = [{
+      state: 'text',
+      text: 'Text'
+      //icon: 'text-format'
+    }, {
+      state: 'text',
+      text: 'Numbers'
+      //icon: 'plus-one'
+    }, {
+      state: 'option',
+      text: 'Checkboxes'
+      //icon: 'check-box'
+    }, {
+      state: 'option',
+      text: 'Switches'
+      //icon: 'radio-button-off'
+    }, {
+      state: 'option',
+      text: 'Radio buttons'
+      //icon: 'radio-button-on'
+    }, {
+      state: 'select',
+      text: 'Selectors'
+      //icon: 'arrow-drop-down'
+    }, {
+      state: 'select',
+      text: 'Dates'
+      //icon: 'today'
+    }, {
+      state: 'wrapper',
+      text: 'Wrapper'
+    }, {
+      state: 'themes',
+      text: 'Themes'
+    }, {
+      state: 'flex-box',
+      text: 'FlexBox'
+    }];
+
+    return {
+      scope: {
+        items: '@'
+      },
+      templateUrl: 'app/nav/sidebar.tmpl.html',
+      controllerAs: 'vm',
+      bindToController: true,
+      controller: function (SidebarService) {
+        var vm = this;
+        vm.sidebarLinks = sidebarLinks;
+        vm.sidebar = SidebarService;
+      }
+    };
+  }
+
+}());
